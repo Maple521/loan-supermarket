9fd94a94e327e043cf39664fee4546a008cd7309
whb <wanghongbo941025>
2019-05-03 12:31:26 +0800

    springcloud项目初始化

--- gani-admin/src/main/java/com/maple/gani/service/TestService.java
+++
@@ -1,6 +0,0 @@
-package com.maple.gani.service;
-
-public interface TestService {
-
-    String getInfo(String str);
-}
--- gani-user/src/main/java/com/maple/gani/controller/HelloController.java
+++ gani-user/src/main/java/com/maple/gani/controller/HelloController.java
@@ -11,7 +11,7 @@ import javax.annotation.Resource;
 @RestController
 public class HelloController {
 
-    @Value(value = "whb")
+    @Value("${whb}")
     private String whb;
 
     @Resource
