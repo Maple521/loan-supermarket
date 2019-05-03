b0036da5dc411fbfc03e88a4ab7d1afa2609fa73
whb <wanghongbo941025>
2019-05-03 22:09:19 +0800

    springcloud项目初始化

--- gani-admin/src/main/java/com/maple/gani/controller/HelloController.java
+++ gani-admin/src/main/java/com/maple/gani/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.maple.gani.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,12 +19,22 @@ public class HelloController {
     @Resource
     private DiscoveryClient client;
 
+
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String index() {
         List<ServiceInstance> instances = client.getInstances("gani-admin");
-        for(ServiceInstance instance : instances){
+        for (ServiceInstance instance : instances) {
             logger.info("/hello，host:" + instance.getHost() + ",service_id:" + instance.getServiceId());
         }
         return "Hello World";
     }
+
+    @Value("${whb}") // git配置文件里的key
+    String whb;
+
+    @RequestMapping(value = "/hi")
+    public String hi() {
+        return whb;
+    }
+
 }
